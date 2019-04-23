f18d9d04de5d8ef7efa42a1a9a2d4be78716230b
Joseph Vazhappilly <joseph.vazhappilly.paily@huawei.com>
2019-04-23 16:41:32 +0530

    Refactor provision volume actions

--- contrib/st2/opensds/actions/create_volume.py
+++ contrib/st2/opensds/actions/create_volume.py
@@ -1,3 +1,17 @@
+# Copyright 2019 The OpenSDS Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import requests
 import json
 
@@ -5,16 +19,35 @@ from st2common.runners.base_action import Action
 
 
 class CreateVolumeAction(Action):
-
-    def run(self, url="", name="", size=1):
+    def run(self,
+            ipaddr="",
+            port="",
+            projectid="",
+            name="",
+            description="Volume",
+            availabilityzone="default",
+            profileid="",
+            snapshotid="",
+            snapshotfromcloud="",
+            size=1):
         data = {
             "Name": name,
-            "Description": "Test Volume from StackStorm",
+            "Description": description,
+            "AvailabilityZone": availabilityzone,
+            "ProfileId": profileid,
+            "SnapshotId": snapshotid,
+            "SnapshotFromCloud": snapshotfromcloud,
             "Size": size
             }
+        url = "http://" + \
+            ipaddr + ":" + \
+            port + "/v1beta/" + \
+            projectid + "/block/volumes"
+
         headers = {'content-type': 'application/json'}
         r = requests.post(url=url, data=json.dumps(data), headers=headers)
         print(r.status_code)
+        r.raise_for_status()
         print(r.text)
         resp = r.json()
         return resp["id"]
@@ -23,4 +56,4 @@ class CreateVolumeAction(Action):
 if __name__ == '__main__':
 
     volumeId = CreateVolumeAction()
-    print('%s : %s' % 'Volume created with Id', volumeId)
+    print('%s : %s' % ('Volume created with Id', volumeId))
--- contrib/st2/opensds/actions/list_volume.py
+++ contrib/st2/opensds/actions/list_volume.py
@@ -1,11 +1,25 @@
+# Copyright 2019 The OpenSDS Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import requests
 
 from st2common.runners.base_action import Action
 
 
 class ListVolumeAction(Action):
-    def run(self, ip="", port="", projectid=""):
-        url = "http://"+ip+":"+port+"/v1beta/"+projectid+"/block/volumes"
+    def run(self, ipaddr="", port="", projectid=""):
+        url = "http://"+ipaddr+":"+port+"/v1beta/"+projectid+"/block/volumes"
         r = requests.get(url=url)
         print(r.status_code)
         r.raise_for_status()
