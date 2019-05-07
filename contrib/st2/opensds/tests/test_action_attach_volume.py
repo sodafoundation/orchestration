f855e21b4433baa553810a89d885e868608ae500
Joseph Vazhappilly <joseph.vazhappilly.paily@huawei.com>
2019-05-07 17:01:02 +0530

    Add unit test for provision volume workflow

---
+++ contrib/st2/opensds/tests/test_action_attach_volume.py
@@ -0,0 +1,59 @@
+# Copyright 2019 The OpenSDS Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from st2tests.base import BaseActionTestCase
+from attach_volume import AttachVolumeAction
+import mock
+
+
+class AttachVolumeActionTestCase(BaseActionTestCase):
+    action_cls = AttachVolumeAction
+
+    def test_run(self):
+        with mock.patch('requests.post') as mock_post:
+            # Configure the mock to return a response
+            mock_post.return_value.status_code = 200
+            mock_post.return_value.json.return_value = {}
+
+            action = self.get_action_instance()
+
+            response = action.run(
+                ip_addr="127.0.0.1",
+                port="5000",
+                tenant_id="123",
+                volume_id="vol_name",
+                host_info={
+                    "host": "ubuntu",
+                    "initiator": "iqn.1993-08.org.debian:01:437bac3717c8",
+                    "ip": "100.64.41.133"
+                    },
+                access_protocol="iscsi",
+                token="12345")
+
+            self.assertEqual(response, None)
+
+            expected_data = '{"ConnectionInfo": "", ' + \
+                '"HostInfo": {"ip": "100.64.41.133", ' + \
+                '"host": "ubuntu", ' + \
+                '"initiator": "iqn.1993-08.org.debian:01:437bac3717c8"}, ' + \
+                '"AccessProtocol": "iscsi", "VolumeId": "vol_name", ' + \
+                '"TenantId": "123", "Mountpoint": ""}'
+
+            mock_post.assert_called_once_with(
+                url='http://127.0.0.1:5000/v1beta/123/block/attachments',
+                data=expected_data,
+                headers={
+                    'content-type': 'application/json',
+                    'x-auth-token': '12345'}
+            )
---
+++ contrib/st2/opensds/tests/test_action_create_volume.py
@@ -0,0 +1,52 @@
+# Copyright 2019 The OpenSDS Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from st2tests.base import BaseActionTestCase
+from create_volume import CreateVolumeAction
+from nose.tools import assert_is_not_none
+import mock
+
+
+class CreateVolumeActionTestCase(BaseActionTestCase):
+    action_cls = CreateVolumeAction
+
+    def test_run(self):
+        with mock.patch('requests.post') as mock_post:
+            # Configure the mock to return a response
+            mock_post.return_value.status_code = 200
+            res = {"id": "12345"}
+            mock_post.return_value.json.return_value = res
+
+            action = self.get_action_instance()
+
+            response = action.run(
+                ip_addr="127.0.0.1", port="5000",
+                tenant_id="123", name="vol_name", size=1, token="12345")
+
+            self.assertEqual(response, "12345")
+            expected_data = '{"AvailabilityZone": "default", ' + \
+                '"Name": "vol_name", "SnapshotFromCloud": "", ' + \
+                '"profile_id": "", "SnapshotId": "", "Size": 1, ' + \
+                '"Description": "Volume"}'
+
+            mock_post.assert_called_once_with(
+                url='http://127.0.0.1:5000/v1beta/123/block/volumes',
+                data=expected_data,
+                headers={
+                    'content-type': 'application/json',
+                    'x-auth-token': '12345'}
+            )
+            # If the request is sent successfully,
+            # then a response to be returned.
+            assert_is_not_none(response)
